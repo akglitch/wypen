9ff1cc3002a4b28b2c3bfcc0bb859f9489b10f2e
kaytee <https://github.com/akglitch/kayteeestate.git>
2023-10-12 20:02:09 +0000

    added footer.js

---
+++ components/Footer.tsx
@@ -0,0 +1,40 @@
+function Footer() {
+    return ( 
+        <>
+      
+
+  
+<hr className=" mx-5" />
+  <footer className=" pb-5">
+  <div className="max-w-screen-xl px-4 pt-8 mx-auto sm:px-6 lg:px-8">
+    <div className="sm:flex sm:items-center sm:justify-between">
+      <div className="flex justify-center text-teal-300 sm:justify-start">
+      <a
+           className="btn  m-1"
+           href="#!"
+           role="button"
+           ><i className="fa fa-twitter-square text-blue-400 ">twitter</i
+          ></a>
+
+       
+
+       
+        <a
+           className="btn  m-1"
+           href="#!"
+           role="button"
+           ><i className="fa fa-instagram text-blue-400" >instagram</i></a>
+
+      </div>
+
+      <p className="mt-4 text-sm text-center text-gray-400 lg:text-right lg:mt-0">
+        T&C &nbsp; Career &nbsp; Privacy & Policy &nbsp; Developers
+      </p>
+    </div>
+  </div>
+</footer>
+        </>
+     );
+}
+
+export default Footer;
