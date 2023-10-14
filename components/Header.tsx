6b46f99168c22fc8e9be8caebd0776325864d87f
kaytee <https://github.com/akglitch/kayteeestate.git>
2023-10-14 09:08:55 +0000

    simple tweaksssss

--- components/Header.tsx
+++ components/Header.tsx
@@ -35,61 +35,49 @@ function Header() {
     <>
       <div className="h-screen w-full overflow-hidden flex flex-nowrap text-center" id="slider">
         <motion.div
-          className="space-y-4 flex-none w-full flex flex-col items-center justify-center"
+          className="space-y-4 flex-none w-full flex flex-col items-center justify-center bg-cover relative"
+          style={{ backgroundImage: 'url("pic3.jpg")', height: '90%' }}
           variants={stagger}
           initial="hidden"
           animate="visible"
         >
-          <motion.h2
-            className="text-4xl max-w-md "
-            variants={fadeIn}
-          >
+          <motion.h2 className="text-4xl max-w-md text-white" variants={fadeIn}>
             Your Big Idea
           </motion.h2>
-          <motion.p
-            className="max-w-md "
-            variants={fadeIn}
-          >
-            It&apos;s fast, flexible, and reliable — with zero-runtime.
+          <motion.p className="max-w-md text-white" variants={fadeIn}>
+            It's fast, flexible, and reliable — with zero-runtime.
           </motion.p>
+          <i className="fa fa-arrow-circle-o-down text-white text-5xl absolute bottom-4 animate-bounce cursor-pointer"></i>
         </motion.div>
         <motion.div
-          className="space-y-4 flex-none w-full flex flex-col items-center justify-center"
+          className="space-y-4 flex-none w-full flex flex-col items-center justify-center bg-cover relative"
+          style={{ backgroundImage: 'url("pic2.jpg")', height: '90%' }}
           variants={stagger}
           initial="hidden"
           animate="visible"
         >
-          <motion.h2
-            className="text-4xl max-w-md "
-            variants={fadeIn}
-          >
+          <motion.h2 className="text-4xl max-w-md text-white" variants={fadeIn}>
             Tailwind CSS works by scanning all of your HTML
           </motion.h2>
-          <motion.p
-            className="max-w-md "
-            variants={fadeIn}
-          >
-            It&apos;s fast, flexible, and reliable — with zero-runtime.
+          <motion.p className="max-w-md text-white" variants={fadeIn}>
+            It's fast, flexible, and reliable — with zero-runtime.
           </motion.p>
+          <i className="fa fa-arrow-circle-o-down text-white text-5xl absolute bottom-4 animate-bounce cursor-pointer"></i>
         </motion.div>
         <motion.div
-          className="space-y-4 flex-none w-full flex flex-col items-center justify-center"
+          className="space-y-4 flex-none w-full flex flex-col items-center justify-center bg-cover relative"
+          style={{ backgroundImage: 'url("pic.jpg")', height: '90%' }}
           variants={stagger}
           initial="hidden"
           animate="visible"
         >
-          <motion.h2
-            className="text-4xl max-w-md "
-            variants={fadeIn}
-          >
+          <motion.h2 className="text-4xl max-w-md text-white" variants={fadeIn}>
             React, Vue, and HTML
           </motion.h2>
-          <motion.p
-            className="max-w-md "
-            variants={fadeIn}
-          >
-            Accessible, interactive examples for React and Vue powered by Headless UI, plus vanilla HTML if you&apos;d rather write any necessary JS yourself.
+          <motion.p className="max-w-md text-white" variants={fadeIn}>
+            Accessible, interactive examples for React and Vue powered by Headless UI, plus vanilla HTML if you'd rather write any necessary JS yourself.
           </motion.p>
+          <i className="fa fa-arrow-circle-o-down text-white text-5xl absolute bottom-4 animate-bounce cursor-pointer"></i>
         </motion.div>
       </div>
     </>
