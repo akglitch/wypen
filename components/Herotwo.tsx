4890eec9efb43635ba857919b8820046c218ce9a
kaytee <https://github.com/akglitch/kayteeestate.git>
2023-10-15 22:28:23 +0000

    simple tweaks

--- components/Herotwo.tsx
+++ components/Herotwo.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 function Herotwo() {
   return ( 
     <div>
-  <div className="text-black text-center p-4 mt-40 mb-40">
-      <p className="text-3xl font-extrabold">
-        The world is an incredibly <span className="text-3xl text-blue-400">luxurious  </span> place.
-      </p>
-      <p className="mt-2 text-xl">
-        It's a realm of opulence, elegance, and boundless possibilities.
-      </p>
-      <p className="mt-2 text-xl">
-        Let's uncover its treasures and embrace its grandeur.
-      </p>
+      <div className="text-black text-center p-4 mt-40 mb-40">
+        <p className="text-3xl font-extrabold">
+          The world is an incredibly <span className="text-3xl text-blue-400">luxurious</span> place.
+        </p>
+        <p className="mt-2 text-xl">
+          It's a realm of opulence, elegance, and boundless possibilities.
+        </p>
+        <p className="mt-2 text-xl">
+          Let&apos;s uncover its treasures and embrace its grandeur.
+        </p>
+      </div>
     </div>
-    </div>
-   );
+  );
 }
 
 export default Herotwo;
