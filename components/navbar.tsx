638d823657dabf4505f0043f85eebea64d12deec
kaytee <https://github.com/akglitch/kayteeestate.git>
2023-11-09 21:26:32 +0000

    simple tweaks

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -112,7 +112,7 @@ export const Navbar = () => {
 										? "danger"
 										: "foreground"
 								}
-								href="#"
+								href={`#${item.href}`}
 								size="lg"
 							>
 								{item.label}
